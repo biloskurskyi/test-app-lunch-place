cc2aef170b4e0253badcd6c313fec8cec046b536
Valerii <v.bill2003@gmail.com>
2024-07-17 11:36:56 +0300

    Ready all api for correct work with user and data.

--- app/menu/urls.py
+++ app/menu/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import MenuView
+from .views import MenuItemView, MenuView
 
 app_name = 'menu'
 
 urlpatterns = [
-    path('menu/', MenuView.as_view(), name='create')
+    path('menu/', MenuView.as_view(), name='menu'),
+    path('menu/<int:pk>/', MenuItemView.as_view(), name='menuItem'),
 ]
--- app/menu/views.py
+++ app/menu/views.py
@@ -22,8 +22,7 @@ class MenuView(APIView):
         return Response({"message": "This user has not this permission"}, status=400)
 
     def get(self, request):
-        # user = request.user
-        menu = Lunch.objects.filter(day=1)
+        menu = Lunch.objects.filter(day=3)
         serializer = LunchSerializer(menu, many=True)
         if not menu.exists():
             return Response({"message": "No menu for today"}, status=200)
@@ -32,3 +31,47 @@ class MenuView(APIView):
             return Response(serializer.data, status=200)
 
         return Response({"message": "This user has not this permission"}, status=403)
+
+
+class MenuItemView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, pk):
+        user = request.user
+        menu_item = Lunch.objects.filter(id=pk).first()
+        if not menu_item or user.user_type != 0:
+            return Response({"error": "This item does not exist"}, status=404)
+
+        serializer = LunchSerializer(menu_item, many=False)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        user = request.user
+        request.data['user'] = user.id
+        menu_item = Lunch.objects.filter(id=pk, user_id=user.id).first()
+        if not menu_item:
+            return Response({"error": "This item does not exist"}, status=404)
+
+        menu = request.data.get('menu', menu_item.menu)
+        day = request.data.get('day', menu_item.day)
+        serializer = LunchSerializer(menu_item, data={'menu': menu, 'day': day},
+                                     partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        user = request.user
+        menu_item = Lunch.objects.filter(id=pk, user_id=user.id).first()
+        if not menu_item:
+            return Response({"error": "Post not found"}, status=404)
+
+        try:
+            menu_item.delete()
+            return Response({
+                'message': 'Delete successful!'
+            }, status=204)
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
